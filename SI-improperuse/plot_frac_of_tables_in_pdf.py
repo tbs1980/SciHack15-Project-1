b21ffba23fb605c707184a0038dbce9e54ec0177
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-11-26 23:13:01 +0000

    removed comas from csv file, added plots

--- SI-improperuse/plot_frac_of_tables_in_pdf.py
+++ SI-improperuse/plot_frac_of_tables_in_pdf.py
@@ -3,13 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # read the csv file
-journal_data_file_name = "./peerj-docx-table-ratio.csv"
+journal_data_file_name = "./peerj-pdf-table-ratio.csv"
 
 # rows 51,52 and 600 had an extra coma in it. changed
 journal_df = pd.DataFrame().from_csv(journal_data_file_name,header=0, sep=',',index_col=0)
 
 # plot the data
-journal_df.plot(kind='hist',histtype='step',bins=10)
+journal_df['fraction'].plot(kind='hist',histtype='step',bins=10)
 
 png_file_name = journal_data_file_name.replace('csv','png')
 plt.savefig(png_file_name)
