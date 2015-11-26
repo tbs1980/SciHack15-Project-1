7631aa04971e057657e21501903b64222d91d481
Derek Groen <djgroennl@gmail.com>
2015-11-26 15:02:28 +0000

    Partially working code now...

--- SI-improperuse/DetectImproper.py
+++ SI-improperuse/DetectImproper.py
@@ -3,32 +3,42 @@
 
 import os
 import sys
+import re
 
-def walk_dir():
- return [x[0] for x in os.walk(".")]
-
-
-def parse_full_html_for_citations(fulltextfile):
-  tag_length = len("<span class=\"metric-counter metric-counter-citing\">")
-
-  inp = open(fulltextfile, 'r')
-  for line in inp:
-    if "metric-counter-citing" in line:
-      cites = line.replace("</span></a></li>","")
-      cites_pos = line.index("<span class=\"metric-counter metric-counter-citing\">") + tag_length
-      num_citations = int(cites[cites_pos:].strip())
-      return num_citations
+def walk_dir(dirname="."):
+ return [x[0] for x in os.walk(dirname)]
 
 
-dirs = walk_dir()
+def file_len(fname):
+  with open(fname) as f:
+    for i, l in enumerate(f):
+      pass
+  return i + 1
+
+def get_num_one_word_sentences(fname):
+  j = 0
+  with open(fname) as f:
+    for i, l in enumerate(f):
+      if len((l.strip()).split()) == 1:
+        j += 1
+  return j
+
+dirs = walk_dir(sys.argv[1])
 for d in dirs:
   # traverse all supplementary info files in a directory.
   for x in os.walk(d):
     for f in x[2]:    
       if f.endswith(".docx"):
         # convert all docx files to plaintext
-        # os.system("~/Software/docx2txt/docx2txt %s/%s" % (d, f))
-        print("~/Software/docx2txt/docx2txt %s/%s" % (d, f))
-        print "%s/%s.txt" % (d, f[:-4])
+        #os.system("~/Software/docx2txt/docx2txt.sh %s/%s" % (d, f))
+        #print("~/Software/docx2txt/docx2txt %s/%s" % (d, f))
+        txt_name = "%s/%s.txt" % (x[0], re.escape(f[:-5]))
+        txt_name = txt_name.replace("\_","_")
+        txt_name = txt_name.replace("\.",".")
+        txt_name = txt_name.replace("\-","-")
+        #txt_name = r"%s" % (txt_name)
+        #if "&" not in txt_name:
+        print txt_name
+        print file_len(txt_name), get_num_one_word_sentences(txt_name)
 
 
